f954aca68a17dc5f1f2ff66af6d8d03d1e719415
wozniackia <wozniackia@gmail.com>
2025-06-25 13:14:03 +0200

    Kolory

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,6 +1,7 @@
 // src/App.tsx
 
-import balanceLogo from "/balance-logo-napis.svg"
+import balanceLogoNapis from "/balance-logo-napis.svg"
+import balanceLogoLudek from "/balance-logo-ludek.svg"
 import "./App.css";
 
 function App() {
@@ -8,8 +9,11 @@ function App() {
     <>
       <div>
         <a href="https://balance-concept.pl/" target="_blank">
-          <img src={balanceLogo} className="logo" alt="Balance Concept" />
+          <img src={balanceLogoNapis} className="logo" alt="Balance Concept" />
         </a>
+        <h1>Pilates Club</h1>
+        <img src={balanceLogoLudek} className="logo" alt="Logo Balance Concept" />
+        <p>© 2025 Balance Concept.</p>
       </div>
     </>
   );
