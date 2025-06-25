3d92eacdb6c54c1ee0801f005eca7c2f8d182f9b
wozniackia <wozniackia@gmail.com>
2025-06-25 13:32:30 +0200

    styling

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,16 +1,18 @@
 // src/App.tsx
 
-import balanceLogoNapis from "/balance-logo-napis.svg"
-import balanceLogoLudek from "/balance-logo-ludek.svg"
+//import balanceLogoNapis from "/balance-logo-napis.svg"
+//import balanceLogoLudek from "/balance-logo-ludek.svg"
+import balanceLogo from "/balance-logo.svg"
 import "./App.css";
 
 function App() {
   return (
     <>
       <div>
-        <img src={balanceLogoNapis} className="logo-napis" alt="Balance Concept" />
-        <br/>
-        <img src={balanceLogoLudek} className="logo-ludek" alt="Logo Balance Concept" />
+        {/*<img src={balanceLogoNapis} className="logo-napis" alt="Balance Concept" />*/}
+        {/*<br/>*/}
+        {/*<img src={balanceLogoLudek} className="logo-ludek" alt="Logo Balance Concept" />*/}
+        <img src={balanceLogo} className="logo-napis" alt="Balance Concept" />
         <p className="copyright">© 2025 Balance Concept.</p>
       </div>
     </>
