db6abfe49772f634945a6acad7c0cb1d6be36b41
wozniackia <wozniackia@gmail.com>
2025-06-25 13:30:17 +0200

    styling

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -9,6 +9,7 @@ function App() {
     <>
       <div>
         <img src={balanceLogoNapis} className="logo-napis" alt="Balance Concept" />
+        <br/>
         <img src={balanceLogoLudek} className="logo-ludek" alt="Logo Balance Concept" />
         <p className="copyright">© 2025 Balance Concept.</p>
       </div>
